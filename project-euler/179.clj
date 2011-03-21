dfe09d3f56c9750395f45682f3ad12a0dceb3ddd
Swizec Teller <swizec@swizec.com>
2011-03-21 15:27:17 +0100

    properly using memoize seems to make things a dash faster

---
+++ project-euler/179.clj
@@ -0,0 +1,39 @@
+
+(defn smallest-div [n]
+  (loop [i 2]
+    (if (zero? (rem n i)) i
+	(recur (inc i)))))
+
+(def factorize 
+  (memoize (fn [n]
+	     (if (<= n 1) nil
+		 (let [d (smallest-div n)]
+		   (concat [d] (factorize (/ n d))))))))
+
+	  
+;(println (factorize 5350))
+
+(defn divisors [n]
+  (let [facts (factorize n)]
+    (loop [n 1
+	   last 0
+	   f facts
+	   acc 1]
+      (let [f1 (first f)]
+	(if (empty? f) acc
+	    (if (or (= f1 last) (= nil f1))
+	      (recur (inc n) f1 (next f) acc)
+	      (recur 1 f1 (next f) (* acc (inc n)))))))))
+	    
+  
+;(println (divisors 28))
+
+;(memoize divisors)
+
+(defn many-divisors [max]
+  (loop [i 2]
+    (divisors i)
+    (if (> i max) true
+	(recur (inc i)))))
+
+(many-divisors 1000)
