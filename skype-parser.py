d8a5ff81d4b4f71029fbaab0295c81afe2dc59c8
Swizec Teller <swizec@swizec.com>
2011-07-26 12:53:15 -0700

    counter thing

--- skype-parser.py
+++ skype-parser.py
@@ -5,4 +5,8 @@ from BeautifulSoup import BeautifulSoup
 
 soup = BeautifulSoup(open(FILE).read())
 
-print soup.pretify()
+# roughly counts number of words the Remote has written on Skype
+print sum([s.count(' ') for s in
+              [dt.nextSibling.string if dt.nextSibling.string else ''
+                        for dt in filter(lambda dt: dt['class'] == 'local',
+                                         soup.findAll('dt'))]])
