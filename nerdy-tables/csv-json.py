d61bc19d04cb2949bce9db7a0f6122414147dc36
Swizec Teller <swizec@swizec.com>
2011-10-02 23:52:41 +0200

    collapsing levels into range

--- nerdy-tables/csv-json.py
+++ nerdy-tables/csv-json.py
@@ -15,13 +15,24 @@ def extract(data, start):
         def clean(item):
             item = map(lambda x: int(x) if x != None and x != '' else 0,
                        list(item))
+            if len(item) < 3:
+                item.append(0)
             return sum(item) if combine else item
 
         return list(flatten([map(clean, grouper(2, row[0:27])),
                              map(clean, grouper(3, row[28:123])),
                              map(clean, grouper(3, row[127:144]))]))
+    def combine(rows):
+        def col(n, day):
+            return [d[n] for d in day]
+        def _index(val, data):
+            return data.index(val) if val in data else -1
+        return [[_index(1, col(0, [row[i] for row in rows])),
+                 _index(1, col(1, [row[i] for row in rows])),
+                 _index(1, col(2, [row[i] for row in rows]))]
+                for i in xrange(len(rows[0]))]
 
-    return {'horny': [days(r[1:]) for r in data[start:start+6]],
+    return {'horny': combine([days(r[1:]) for r in data[start:start+6]]),
             'cyber': days(data[start+7][1:], True),
             'touchy': days(data[start+8][1:], True)}
 
@@ -30,4 +41,4 @@ if __name__ == '__main__':
 
     swizec = extract(data, 6)
 
-    print swizec['touchy']
+    print swizec['horny']
