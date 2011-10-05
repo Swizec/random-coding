1e80c778c2c809c68989a58a19525caa2eb3a5a6
Swizec Teller <swizec@swizec.com>
2011-10-06 01:09:14 +0200

    finally ironed out all the kinks

--- nerdy-tables/csv-json.py
+++ nerdy-tables/csv-json.py
@@ -3,7 +3,7 @@ import csv, collections, json
 from itertools import izip_longest, chain
 
 def extract(data, start):
-    def grouper(n, iterable, fillvalue=None):
+    def grouper(n, iterable, fillvalue=0):
         "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
         args = [iter(iterable)] * n
         return list(izip_longest(fillvalue=fillvalue, *args))
@@ -19,35 +19,37 @@ def extract(data, start):
         def clean(item):
             item = map(lambda x: int(x) if x != None and x != '' else 0,
                        list(item))
-            if len(item) < 3:
-                item.append(0)
+            item += [0]*(3-len(item))
             return sum(item) if combine else item
 
-        return list(flatten([map(clean, grouper(2, row[col('B'):col('BC')])),
-                             map(clean, grouper(3, row[col('BD'):col('EU')])),
-                             map(clean, grouper(3, row[col('EY'):col('FP')]))]))
+        return list(flatten([map(clean, grouper(2, row[col('B'):col('BD')])),
+                             map(clean, grouper(3, row[col('BD'):col('EV')])),
+                             map(clean, grouper(3, row[col('EY'):col('FQ')]))]))
 
     def combine(rows):
-        def col(n, day):
+        def _col(n, day):
             return [d[n] for d in day]
         def _index(val, data):
             return data.index(val)+1 if val in data else -1
-        l = max([len(r) for r in rows])
-        rows = [r+[[0,0,0]]*(l-len(r)) for r in rows]
-        return [[_index(1, col(0, [row[i] for row in rows])),
-                 _index(1, col(1, [row[i] for row in rows])),
-                 _index(1, col(2, [row[i] for row in rows]))]
+
+        l = len(rows[0])
+
+        return [[_index(1, _col(0, [row[i] for row in rows])),
+                 _index(1, _col(1, [row[i] for row in rows])),
+                 _index(1, _col(2, [row[i] for row in rows]))]
                 for i in xrange(l)]
 
-    l = max([len(r) for r in data[21:30]])
-    cleaned = [r+[0]*(l-len(r)) for r in data[start:start+9]]
+    cleaned = data[start:start+9]
 
-    return {'horny': combine([days(r) for r in cleaned[:6]]),
+    return {'horny': combine([days(r) for r in cleaned[:7]]),
             'cyber': days(cleaned[7], True),
             'touchy': days(cleaned[8], True)}
 
 if __name__ == '__main__':
     data = list(csv.reader(open("jun-aug.csv", 'rb')))
+
+    max_len = max([len(r) for r in data])
+    data = [r+['']*(max_len-len(r)) for r in data]
 
     swizec = extract(data, 6)
     masa = extract(data, 21)
