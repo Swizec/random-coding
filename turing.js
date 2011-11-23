d6c0b88c1cce49ff33675439ab3b99d4f36cb905
Swizec Teller <swizec@swizec.com>
2011-11-23 01:48:15 +0100

    a simple turing machine implemented

--- turing.js
+++ turing.js
@@ -1,21 +1,29 @@
 
 // transition format: {from: 'q1', to: 'q2', via: 'A', put: 'B', move: 1/-1}
-// needs list of instructions
-// needs start and end state
-// needs word
-// implements delta function
 // spits out end word
+// basic way to run:  node turing.js machine.json 111 q0 q4
+// node turing.js <instructions> <word> <start> <end>
 
 var tape = process.argv[3];
 var instructions = JSON.parse(require('fs').readFileSync(process.argv[2], 'utf-8'));
 var start = process.argv[4];
 var stop = process.argv[5];
 
-var delta = function (tape, inst, cur, end, states) {
-    states = states || {};
-    // build map of states as you go (memoization of sorts)
-    // perform changes on tape as per current instruction
+var delta = function (tape, states, state, i, end) {
+    if (state == end) return tape;
+    var cur = (tape[i]) ? states[state][tape[i]] : states[state]['eps'];
 
+    if (cur) {
+        if (cur.put != "eps") tape.splice(i, 1, cur.put);
+        tape = delta(tape, states, cur.to, i+cur.move, end);
+    }
+    return tape;
 };
 
-console.log(delta(tape, instructions, start, stop));
+var states = {};
+instructions.map(function (instr) {
+    states[instr.from] = states[instr.from] || {};
+    states[instr.from][instr.via] = instr;
+});
+
+console.log(delta(tape.split(""), states, start, 0, stop).join(""));
